da9c762ca6e606bb720e7ae7d91a67a09c006c81
Jamie <bluegigsolutions@gmail.com>
2018-08-29 18:35:46 +0100

    SQLite Db test

--- app/src/main/java/uk/co/jmwdesigns/easysupplement/EasySupplementDataBaseHelper.java
+++ app/src/main/java/uk/co/jmwdesigns/easysupplement/EasySupplementDataBaseHelper.java
@@ -1,8 +1,11 @@
 package uk.co.jmwdesigns.easysupplement;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+
+import static java.sql.Types.INTEGER;
 
 public class EasySupplementDataBaseHelper extends SQLiteOpenHelper {
 
@@ -10,21 +13,35 @@ public class EasySupplementDataBaseHelper extends SQLiteOpenHelper {
 
     private static final int DB_VERSION = 1;
 
-    EasySupplementDataBaseHelper(Context context){
+    EasySupplementDataBaseHelper(Context context) {
         super(context, DB_NAME, null, DB_VERSION);
-
-
     }
 
 
+
     @Override
-    public void onCreate(SQLiteDatabase sqLiteDatabase) {
-
-
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("CREATE TABLE SUPPLEMENT(_id INTEGER PRIMARY KEY AUTOINCREMENT, "
+                + "NAME TEXT, "
+                + "DESCRIPTION TEXT, "
+                + "RECOMMENDED_USE TEXT, "
+                + "IMAGE_RESOURCE_ID INTEGER) ;");
+        insertSupplement(db, "Whey proten", "Whey Protein", R.drawable.protein_home_pic, "This is the use");
+        insertSupplement(db, "Soy Protein", "Soy proten Description" , R.drawable.protein_home_pic, "This is the use");
     }
 
     @Override
-    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int oldVersion, int newVersion) {
+
+    }
+
+    private static void insertSupplement(SQLiteDatabase db, String name, String description, int resourceId, String recommendedUse){
+        ContentValues supplementValues = new ContentValues();
+        supplementValues.put("NAME", name);
+        supplementValues.put("DESCRIPTION", description);
+        supplementValues.put("IMAGE_RESOURCE_ID", resourceId);
+        supplementValues.put("RECOMMENDED_USE", recommendedUse);
+        db.insert("SUPPLEMENT", null, supplementValues);
 
     }
 }
